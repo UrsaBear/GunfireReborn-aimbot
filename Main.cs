91ae4d00fa14aaa05ffb2829a7d80e3e13932e82
UrsaBear <vorcalion@outlook.com>
2020-10-31 18:50:29 +0800

    允许通过按键常开瞄准

--- Main.cs
+++ Main.cs
@@ -30,6 +30,7 @@ namespace zcMod
         public static bool showallobjinfo = false;
         public static bool limitangle = true;
         public static bool shownpc = false;
+        public static bool aimEnabled = true;
         public override void OnApplicationStart() // Runs after Game Initialization.
         {
             MelonLogger.Log("OnApplicationStart");
@@ -120,9 +121,13 @@ namespace zcMod
                 {
                     limitangle = !limitangle;
                 }
-                
+
                 //if (Input.GetMouseButton(1)) 
-                if (Input.GetKey(KeyCode.F))//按F键自瞄(请按个人喜好修改)
+                if (Input.GetKeyUp(KeyCode.T))
+                {
+                    aimEnabled = !aimEnabled;
+                }
+                if ((aimEnabled && !Input.GetKey(KeyCode.F)) || (!aimEnabled && Input.GetKey(KeyCode.F)))//按F键自瞄(请按个人喜好修改)
                 {
                     List<NewPlayerObject> monsters = NewPlayerManager.GetMonsters();
                     if (monsters != null)
